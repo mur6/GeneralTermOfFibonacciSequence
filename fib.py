08b5aa2692f0c6abd8db847e8c142810ecf8dbd4
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-22 18:19:40 +0900

    fix

--- fib.py
+++ fib.py
@@ -3,7 +3,7 @@ from sympy import sqrt, expand, fibonacci
 
 n = 1000
 
-#answer = (((1 + sqrt(5)) / 2) ** n - ((1 - sqrt(5)) / 2) ** n) / sqrt(5)
-#print(expand(answer))
+answer = (((1 + sqrt(5)) / 2) ** n - ((1 - sqrt(5)) / 2) ** n) / sqrt(5)
+print(expand(answer))
 
 print(fibonacci(1000))
